4840b7275ceb2618187ac1678319363e484e49e2
kys-6 <kys-6@kys-6deMac-Pro.local>
2017-06-14 13:01:20 +0800

    zzz

---
+++ 学习github/fff.h
@@ -0,0 +1,13 @@
+//
+//  fff.h
+//  学习github
+//
+//  Created by kys-6 on 2017/6/14.
+//  Copyright © 2017年 kys-5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fff : UIView
+
+@end
---
+++ 学习github/fff.m
@@ -0,0 +1,21 @@
+//
+//  fff.m
+//  学习github
+//
+//  Created by kys-6 on 2017/6/14.
+//  Copyright © 2017年 kys-5. All rights reserved.
+//
+
+#import "fff.h"
+
+@implementation fff
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
